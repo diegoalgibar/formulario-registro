d9eb66bf80c61b4fdaddb76f112cd0c504206163
diegoalgibar <diegoalgibar123@gmail.com>
2023-08-10 18:42:14 -0300

    Validacion de datos funcional

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,20 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+ document.getElementById("regBtn").addEventListener("click",()=>{
+
+        if (document.getElementById("nombre").value!=""
+            &&document.getElementById("apellido").value!=""
+            &&document.getElementById("email").value!=""
+            &&document.getElementById("password1").value!=""
+            &&document.getElementById("password2").value!=""
+            &&document.getElementById("terminos").checked
+            &&document.getElementById("password1").value==document.getElementById("password2").value
+            &&document.getElementById("password1").value.length>=6) {
+            showAlertSuccess();
+        } else {
+            showAlertError();
+        }
+    
+})
