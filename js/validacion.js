fbb41534610f800bec33f02b4b331f517ffea5e9
diegoalgibar <diegoalgibar123@gmail.com>
2023-08-10 20:55:47 -0300

    Correccion queryselectorall

--- js/validacion.js
+++ js/validacion.js
@@ -6,14 +6,19 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-document.getElementById("regBtn").addEventListener("click",()=>{
-    if (    document.querySelectorAll("input").value!=""
+ document.getElementById("regBtn").addEventListener("click",()=>{
+
+        if (document.getElementById("nombre").value!=""
+            &&document.getElementById("apellido").value!=""
+            &&document.getElementById("email").value!=""
+            &&document.getElementById("password1").value!=""
+            &&document.getElementById("password2").value!=""
             &&document.getElementById("terminos").checked
             &&document.getElementById("password1").value==document.getElementById("password2").value
             &&document.getElementById("password1").value.length>=6) {
-                showAlertSuccess();
-     } else {
-                showAlertError();
-    }
+            showAlertSuccess();
+        } else {
+            showAlertError();
+        }
     
-});
+})
