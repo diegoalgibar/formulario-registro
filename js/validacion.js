b0efcbb24991a975f4a3b2fd34c106e09e801a4f
diegoalgibar <diegoalgibar123@gmail.com>
2023-08-10 18:49:02 -0300

    Uso de querySelectorAll

--- js/validacion.js
+++ js/validacion.js
@@ -6,19 +6,14 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
- document.getElementById("regBtn").addEventListener("click",()=>{
-
-        if (document.getElementById("nombre").value!=""
-            &&document.getElementById("apellido").value!=""
-            &&document.getElementById("email").value!=""
-            &&document.getElementById("password1").value!=""
-            &&document.getElementById("password2").value!=""
+document.getElementById("regBtn").addEventListener("click",()=>{
+    if (    document.querySelectorAll("input").value!=""
             &&document.getElementById("terminos").checked
             &&document.getElementById("password1").value==document.getElementById("password2").value
             &&document.getElementById("password1").value.length>=6) {
-            showAlertSuccess();
-        } else {
-            showAlertError();
-        }
+                showAlertSuccess();
+     } else {
+                showAlertError();
+    }
     
-})
+});
